77f80fdff09d154542e21acc96d794cffc4bc8ba
Aaditya Paithane <51539518+aadityamp01@users.noreply.github.com>
2023-01-23 23:08:55 +0530

    activity_complaints.xml completed

--- app/src/main/java/com/aaditya/humaranagar/ComplaintsActivity.kt
+++ app/src/main/java/com/aaditya/humaranagar/ComplaintsActivity.kt
@@ -2,10 +2,19 @@ package com.aaditya.humaranagar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.aaditya.humaranagar.databinding.ActivityComplaintsBinding
 
 class ComplaintsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityComplaintsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_complaints)
+
+        binding = ActivityComplaintsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.toolbarHome.setTitleTextColor(resources.getColor(android.R.color.black));
+        binding.toolbarHome.title = resources.getString(R.string.page_title)
     }
 }
